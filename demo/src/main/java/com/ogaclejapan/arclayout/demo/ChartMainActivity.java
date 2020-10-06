4ff81833c847136cf1bcbdc6a7c3e62693027249
Nishant Shah <nishant.shah@wwindia.com>
2020-10-06 22:26:42 +0530

    fixes

---
+++ demo/src/main/java/com/ogaclejapan/arclayout/demo/ChartMainActivity.java
@@ -0,0 +1,14 @@
+package com.ogaclejapan.arclayout.demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChartMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_main);
+    }
+}
